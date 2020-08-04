5d687171a02ea3486b855751ff15ae632a223f84
Ismael Abu-jadur García <i62abgai@uco.es>
2020-08-04 17:23:48 +0100

    Author and Book forms added

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,6 @@
+from catalog.models import Author
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre, Language
 from django.views import generic
@@ -109,3 +112,35 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death': '05/01/2018'}
+
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+
+
+# Book creating, update and delete
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = ['title', 'author', 'summary', 'isbn', 'genre', 'language']
+
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = reverse_lazy('books')
